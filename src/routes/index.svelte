d1b54f5c596efd7921a372fb6281c253f3a1dfe5
choinashil <choinashil@gmail.com>
2022-02-01 22:45:31 +0900

    test download image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,32 @@
+<script>
+  import { toJpeg } from 'html-to-image';
+
+  function handleClick() {
+    toJpeg(document.getElementById('container'))
+      .then(dataUrl => {
+        const link = document.createElement('a');
+        link.download = 'test-download.jpeg';
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch(err => console.log(err));
+  }
+</script>
+
+<div id='container'>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+<button type='button' on:click={handleClick}>다운로드</button>
+
+
 
 <style lang="scss">
+  #container {
+    background: yellow;
+    border: 3px solid red;
+    width: 500px;
+  }
   h1 {
     color: blue;
   }
